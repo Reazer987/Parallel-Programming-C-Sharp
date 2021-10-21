a444330652aa6f70b20cdec939331f588f4e3d48
Теодор Вълчев <teo.thenx@gmail.com>
2021-10-21 19:02:59 +0300

    Homework 2

---
+++ Homework 2/Online Shop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Online_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Въведете брой купувачи:" + " " + "Максимален Брой 100");
+            int buyers = int.Parse(Console.ReadLine());
+            List<int> buyersList = new List<int>();
+
+            for (int i = 1; i <= buyers; i++)
+            {
+                if (i == 100)
+                {
+                    Console.WriteLine("Достигнахте максималения капацитет");
+                    break;
+                }
+                buyersList.Add(i);
+            }
+            Console.WriteLine(" Въведете брой доставчици:" + " " + "Максимален Брой 5");
+            List<int> suplyersList = new List<int>();
+            int suplyers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= suplyers; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("Достигнахте максимления капацитет");
+                    break;
+                }
+
+                suplyersList.Add(i);
+            }
+
+            Console.WriteLine(" Въведете брой поръчки:" + " " + "Максимален Брой 20");
+            List<int> ordersList = new List<int>();
+            int orders = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= orders; i++)
+            {
+                if (i == 20)
+                {
+                    Console.WriteLine("Достигнахте максималния капацитет");
+                    break;
+                }
+
+                ordersList.Add(i);
+            }
+
+            Store store = new Store(buyersList, suplyersList, ordersList);
+            Console.WriteLine("Брой купувачи:" + string.Join(" ", store.Customers.Max()));
+            Console.WriteLine("Брой доставчици:" + string.Join(" ", store.Suplyers.Max()));
+            Console.WriteLine("Брой поръчките:" + string.Join(" ", store.Orders).Max());
+        }
+    }
+}
---
+++ Homework 2/Online Shop/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Online_Shop
+{
+   public class Store
+    {
+        public Store(List<int> customers, List<int> suplyers, List<int> orders)
+        {
+            Customers = customers;
+            Suplyers = suplyers;
+            Orders = orders;
+        }
+
+        public List<int>  Customers { get; set; }
+        public List<int> Suplyers { get; set; }
+        public List<int> Orders { get; set; }
+
+       
+    }
+}
